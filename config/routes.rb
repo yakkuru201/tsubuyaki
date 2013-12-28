a17c2ab50dac2fb50f3bb63c577b03a68aceb313
yakkuru201 <tonamimagic@gmail.com>
2013-12-28 17:53:13 +0900

    rails g scaffold tweets user_id:integer content:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tsubuyaki::Application.routes.draw do
+  resources :tweets
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
