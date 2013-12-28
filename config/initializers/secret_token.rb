db283cbc107997ff184b73051c422c1bdc66aa9b
yakkuru201 <tonamimagic@gmail.com>
2013-12-28 17:28:41 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tsubuyaki::Application.config.secret_key_base = '12e7926415896e69113a1ed62fde37bc99f2f9412767aafaba6c0ace1b4cfbf6efa65276c29e8f9848d4bdaeba19f33c3006af862b78ba7fddd6f9965faca975'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tsubuyaki::Application.config.session_store :cookie_store, key: '_tsubuyaki_session'
