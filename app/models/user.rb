0a9526cbd6815c8da2f283e2f675628c370112d3
yakkuru201 <tonamimagic@gmail.com>
2013-12-31 02:03:33 +0900

    define User#followed?

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ActiveRecord::Base
   has_many :following_users, through: :follows, source: :User
   has_many :inverse_follows, class_name: Follow, foreign_key: :followed_id
   has_many :followed_users, through: :inverse_follows
+
+  def followded? User
+    Follow.exists?(user_id: user.id, followded_id: self.id)
+  end
 end
