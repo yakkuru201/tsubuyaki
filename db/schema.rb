3364d0e6354c2dc3abc8a52cb1ca15c54d1969a8
yakkuru201 <tonamimagic@gmail.com>
2013-12-31 01:32:00 +0900

    マイグレーションした

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131228095432) do
+ActiveRecord::Schema.define(version: 20131230163119) do
 
   create_table "favorites", force: true do |t|
     t.integer  "user_id"
     t.integer  "tweet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "follows", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
